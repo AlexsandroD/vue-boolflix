657c45afab1e7fbde586b7f6870a1769e1ca03f2
AlexsandroD <adrdra88@gmail.com>
2021-12-20 17:24:59 +0100

    movies and tv series

---
+++ src/components/commons/SearchTv.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="search">
+        <h3>serie tv</h3>
+            <ul>
+                <li v-for='(show, index) in tvShow' :key="index">{{show.title}},{{show.original_title}}</li>
+            </ul>
+    </div>
+</template>
+
+<script>
+export default {
+name:"SearchTV",
+data(){
+    return{
+       TvShows:[],
+    }
+},
+props:{
+   shows:[]
+},
+methods:{
+ 
+    
+  
+}
+}
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
--- src/components/macros/Main.vue
+++ src/components/macros/Main.vue
@@ -1,16 +1,77 @@
 <template>
     <div class="container">
-        <Search/>
+        <input type="text" v-model='inputSearch'>
+        <button @click="getMovieApi()">Click me</button>
+        <Search :filmsInput='films'/>
+        <SearchTV :shows='shows'/>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 import Search from"../commons/Search.vue";
+import SearchTV from"../commons/SearchTv.vue";
 export default {
     name:'Main',
     components:{
         Search,
+        SearchTV,
+    },
+    data(){
+        return{
+            films:[],
+            shows:[],
+            inputSearch:"", 
+        }
+    },
+    methods:{
+    getMovieApi () {
+        if(this.inputSearch != ''){
+            
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    api_key:'86b1172b207765fe60ddcff01203c51b',
+                    query:this.inputSearch,
+                    language:'it-IT',
+                }
+            })
+            .then((response) => {
+                console.log(response)
+                this.films = response.data.results 
+                console.log(this.flag)
+            })
+            .catch(function (error) {
+                    console.log(error);
+            })
+        }else{
+            alert('please write a title')
+        }
+    },
+
+    getTvshow(){
+        if(this.inputSearch != ''){
+            
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    api_key:'86b1172b207765fe60ddcff01203c51b',
+                    query:this.inputSearch,
+                    language:'it-IT',
+                }
+            })
+            .then((response) => {
+                console.log(response)
+                this.shows= response.data.results 
+                console.log(this.flag)
+            })
+            .catch(function (error) {
+                    console.log(error);
+            })
+        }else{
+            alert('please write a title')
+        }
+    },
     }
+
 }
 </script>
 
